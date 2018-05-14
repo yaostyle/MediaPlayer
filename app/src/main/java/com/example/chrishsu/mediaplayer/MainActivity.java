2f00b5c5d957ee6ce167262e602ed50d7810144a
Chris Hsu <yaostyle@gmail.com>
2018-05-13 22:12:24 -0700

    Added onCompleteListener

--- app/src/main/java/com/example/chrishsu/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/chrishsu/mediaplayer/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mp;
@@ -13,13 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     public void playMusic(View view) {
         mp = MediaPlayer.create(this, R.raw.crazygame);
         mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Toast.makeText(MainActivity.this, "I'm done!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void stopMusic(View view) {
